134a8a6bdc0c08b0f4b90459f2207f3af3f9dd7a
Igor-Goncharenko <21gis05ru@gmail.com>
2025-10-10 22:38:57 +0500

    chore: choose hp to connect

--- linux_app/src/main.c
+++ linux_app/src/main.c
@@ -1,37 +1,43 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //#include "audio.h"
 #include "discovery.h"
 
 int main(void) {
-    //pulse_audio_t pulse = { 0 };
     discovery_server_t server = { 0 };
 
     headphone_response_t hps[16];
 
-    //if (audio_init(&pulse) != 0) {
-    //    fprintf(stderr, "Failed to init audio\n");
-    //    audio_destroy(&pulse);
-    //    return EXIT_FAILURE;
-    //}
-
-    //audio_destroy(&pulse);
-    
-    discovery_server_init(&server);
-    int cnt = discover_headphones(&server, hps, 16);
-
-    for (int i = 0; i < cnt; i++) {
-        printf("%d) %s %s %s %s\n", i + 1, hps[i].type, hps[i].model, hps[i].id, hps[i].ip_v4);
+    if (discovery_server_init(&server) != 0) {
+        fprintf(stderr, "Failed to init discovery server. errno=%d, strerror=%s\n",
+                errno, strerror(errno));
+        discovery_server_destroy(&server);
+        return EXIT_FAILURE;
     }
 
-    int idx;
-    do {
-    printf("Choose headphones to connect: \n> ");
-    } while (scanf("%d", &idx) != 1 || idx < 1 || idx > cnt);
+    int cnt = discover_headphones(&server, hps, 16);
 
-    idx--;
+    if (cnt > 0) {
+        for (int i = 0; i < cnt; i++) {
+            printf("%d) %s %s %s %s\n", i + 1, hps[i].type, hps[i].model, hps[i].id, hps[i].ip_v4);
+        }
 
-    printf("Connecting to %s %s %s %s\n", hps[idx].type, hps[idx].model, hps[idx].id, hps[idx].ip_v4);
+        int idx;
+        do {
+            printf("Choose headphones to connect[1-%d]: \n> ", cnt);
+        } while (scanf("%d", &idx) != 1 || idx < 1 || idx > cnt);
+
+        idx--;
+
+        printf("Connecting to %s %s %s %s\n", hps[idx].type, hps[idx].model, hps[idx].id, hps[idx].ip_v4);
+    }
+    else {
+        printf("Nothing found\n");
+    }
+
+    discovery_server_destroy(&server);
 
     return EXIT_SUCCESS;
 }

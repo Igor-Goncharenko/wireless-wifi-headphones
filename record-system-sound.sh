1145341fa51d992e9005485797f1939cdab3048e
Igor-Goncharenko <21gis05ru@gmail.com>
2025-10-15 21:52:51 +0500

    chore: delete unused ffmpeg scripts

--- record-system-sound.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-parec --device=alsa_output.pci-0000_03_00.6.analog-stereo.monitor --format=s16le --channels=2 --rate=44100 | lame -r - output.mp3
-# to play you can use `mplayer output.mp3`
--- udp-system-sound.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ffmpeg -f pulse -i default -c:a pcm_s16le -ar 44100 -ac 1 -f wav udp://192.168.0.232:1234

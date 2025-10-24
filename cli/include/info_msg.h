628d371c069ed0ea934eb66319107f6269c62f6a
Igor-Goncharenko <21gis05ru@gmail.com>
2025-10-24 22:00:07 +0500

    chore(cli): Hello msg and command input

---
+++ cli/include/info_msg.h
@@ -0,0 +1,17 @@
+#ifndef INFO_H
+#define INFO_H
+
+const char HELLO_MSG[] =
+"===============================================\n"
+"          Bluetooth Audio Manager CLI          \n"
+"===============================================\n"
+" Welcome! This tool allows you to manage the   \n"
+" connection to your wireless wifi headphones.  \n"
+"                                               \n"
+" Use 'help' or '?' to view available commands. \n"
+"===============================================\n";
+
+const char EXIT_MSG[] =
+"Exiting from wifi headphones cli.\n";
+
+#endif /* INFO_H */
--- cli/src/main.c
+++ cli/src/main.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <signal.h>
+
+#include "info_msg.h"
+
+#define MAX_COMMAND_LEN 256
+
+static volatile sig_atomic_t keep_running = 1;
+
+void signal_handler(int sig) {
+    switch (sig) {
+        case SIGTERM:
+        case SIGINT:
+            printf("\n%s", EXIT_MSG);
+            keep_running = 0;
+            break;
+        default:
+            break;
+    }
+}
 
 int main(void) {
-    printf("Hello, cli!\n");
+    char cmd_buf[MAX_COMMAND_LEN];
+
+    struct sigaction sa;
+    sa.sa_handler = signal_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGTERM, &sa, NULL);
+    sigaction(SIGINT, &sa, NULL);
+
+    printf("%s", HELLO_MSG);
+
+    while (keep_running) {
+        printf("> ");
+        if (fgets(cmd_buf, sizeof(cmd_buf), stdin) != NULL) {
+            size_t len = strlen(cmd_buf);
+            if (len > 0 && cmd_buf[len - 1] == '\n') {
+                cmd_buf[len - 1] = '\0';
+            }
+            printf("cmd='%s'\n", cmd_buf);
+        }
+    }
+
     return EXIT_SUCCESS;
 }

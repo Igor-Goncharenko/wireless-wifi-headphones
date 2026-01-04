1434cee4338cfb18d6ceaf4980d378fa7bf41dd4
Igor-Goncharenko <21gis05ru@gmail.com>
2026-01-04 23:41:58 +0500

    chore(cli): Send cli request to daemon

--- cli/src/main.c
+++ cli/src/main.c
@@ -11,6 +11,7 @@
 #include "info_msg.h"
 
 #define MAX_COMMAND_LEN 256
+#define SOCKET_PATH "/tmp/wifi_headphones_daemon.sock"
 
 static volatile sig_atomic_t s_keep_running = 1;
 
@@ -24,6 +25,45 @@ void signal_handler(int sig) {
         default:
             break;
     }
+}
+
+int send_command_to_daemon(const char *command) {
+    printf("command=\"%s\"\n", command);
+    int sock = 0;
+    struct sockaddr_un addr;
+
+    if ((sock = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        return -1;
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+
+    if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("connect");
+        close(sock);
+        return -1;
+    }
+
+    if (dprintf(sock, "%s\n", command) < 0) {
+        perror("dprintf");
+        close(sock);
+        return -1;
+    }
+
+    shutdown(sock, SHUT_WR);
+
+    char buffer[1024];
+    ssize_t bytes_received = read(sock, buffer, sizeof(buffer) - 1);
+    if (bytes_received > 0) {
+        buffer[bytes_received] = '\0';
+        printf("Response from daemon: \"%s\"\n", buffer);
+    }
+
+    close(sock);
+    return 0;
 }
 
 int main(void) {
@@ -48,6 +88,7 @@ int main(void) {
             char *command = process_command(cmd_buf);
             printf("cmd='%s'; resp='%s'\n", cmd_buf, command);
             if (command) {
+                send_command_to_daemon(command);
                 free(command);
             }
         }

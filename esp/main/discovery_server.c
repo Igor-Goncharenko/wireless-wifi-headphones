80daed295baf333759ef819b4f4b8d0fe4dbb7d3
Igor-Goncharenko <21gis05ru@gmail.com>
2026-01-12 11:31:19 +0500

    chore(esp): Delete esp whitelist (unnecessary feature)

--- esp/main/discovery_server.c
+++ esp/main/discovery_server.c
@@ -29,10 +29,6 @@ headphones_info_t g_device_info = {
     },
 };
 
-static whitelist_entry_t s_whitelist[MAX_WHITELIST_SIZE];
-static int s_whitelist_len = 0;
-static SemaphoreHandle_t s_whitelist_mutex = NULL;
-
 static void init_device_info(void) {
     uint8_t mac[6];
     esp_read_mac(mac, ESP_MAC_WIFI_STA);
@@ -45,85 +41,6 @@ static void init_device_info(void) {
 
     ESP_LOGI(TAG, "Device: name = \"%s\"; mac=\"%s\"; ipv4=\"%s\";", g_device_info.name,
              g_device_info.mac, g_device_info.ipv4);
-}
-
-static void add_to_whitelist(const struct in_addr addr) {
-    if (xSemaphoreTake(s_whitelist_mutex, pdMS_TO_TICKS(100)) == pdFALSE) {
-        ESP_LOGW(TAG, "Failed to take whitelist mutex");
-        return;
-    }
-
-    const time_t now = time(NULL);
-    int i = 0;
-    bool addr_registered = false;
-
-    while (i < s_whitelist_len) {
-        if (memcmp(&s_whitelist[i].addr, &addr, sizeof(struct in_addr)) == 0) {
-            addr_registered = true;
-            s_whitelist[i].timestamp = now;
-        }
-        if (now - s_whitelist[i].timestamp > WHITELIST_TIMEOUT) {
-            s_whitelist_len--;
-            if (i != s_whitelist_len) {
-                memcpy(&s_whitelist[i], &s_whitelist[s_whitelist_len - 1], sizeof(whitelist_entry_t));
-            }
-            i--;
-        }
-        i++;
-    }
-
-    if (!addr_registered && s_whitelist_len < MAX_WHITELIST_SIZE) {
-        s_whitelist[s_whitelist_len].timestamp = now;
-        s_whitelist[s_whitelist_len].addr = addr;
-        s_whitelist_len++;
-    }
-
-    char addr_str[INET_ADDRSTRLEN];
-    for (int i = 0; i < s_whitelist_len; i++) {
-        inet_ntoa_r(s_whitelist[i].addr, addr_str, sizeof(addr_str));
-        ESP_LOGI(TAG, "%d) %s", i + 1, addr_str);
-    }
-
-    xSemaphoreGive(s_whitelist_mutex);
-}
-
-static void update_whitelist(void) {
-    if (xSemaphoreTake(s_whitelist_mutex, pdMS_TO_TICKS(100)) == pdFALSE) {
-        ESP_LOGW(TAG, "Failed to take whitelist mutex");
-        return;
-    }
-
-    const time_t now = time(NULL);
-    int i = 0;
-
-    while (i < s_whitelist_len) {
-        if (now - s_whitelist[i].timestamp > WHITELIST_TIMEOUT) {
-            s_whitelist_len--;
-            if (i != s_whitelist_len) {
-                memcpy(&s_whitelist[i], &s_whitelist[s_whitelist_len - 1], sizeof(whitelist_entry_t));
-            }
-            i--;
-        }
-        i++;
-    }
-
-    xSemaphoreGive(s_whitelist_mutex);
-}
-
-static bool in_whitelist(const struct in_addr addr) {
-    if (xSemaphoreTake(s_whitelist_mutex, pdMS_TO_TICKS(100)) == pdFALSE) {
-        ESP_LOGW(TAG, "Failed to take whitelist mutex");
-        return false;
-    }
-
-    for (int i = 0; i < s_whitelist_len; i++) {
-        if (memcmp(&addr, &s_whitelist[i].addr, sizeof(struct in_addr)) == 0) {
-            return true;
-        }
-    }
-
-    xSemaphoreGive(s_whitelist_mutex);
-    return false;
 }
 
 static void discovery_server_task(void *arg) {
@@ -184,8 +101,6 @@ static void discovery_server_task(void *arg) {
                 if (sendto(sockfd, &g_device_info, sizeof(headphones_info_t), 0,
                           (struct sockaddr *)&client_addr, client_len) < 0) {
                     ESP_LOGE(TAG, "Failed to send response");
-                } else {
-                    add_to_whitelist(client_addr.sin_addr);
                 }
             }
         } else if (recv_len < 0) {
@@ -249,26 +164,19 @@ static void handshake_server_task(void *arg) {
             ESP_LOGI(TAG, "received handshake request: \"%s\"", buffer);
 
             if (strcmp(buffer, HANDSHAKE_REQUEST) == 0) {
-                update_whitelist();
+                ssize_t bytes_sent = sendto(sockfd, HANDSHAKE_RESPONSE, sizeof(HANDSHAKE_RESPONSE),
+                                            0, (struct sockaddr *)&client_addr, client_len);
+                if (bytes_sent < 0) {
+                    ESP_LOGE(TAG, "HANDSHAKE_RESPONSE send failed");
+                }
 
-                if (in_whitelist(client_addr.sin_addr)) {
-                    ssize_t bytes_sent = sendto(sockfd, HANDSHAKE_RESPONSE,
-                                                sizeof(HANDSHAKE_RESPONSE), 0,
-                                                (struct sockaddr *)&client_addr, client_len);
-                    if (bytes_sent < 0) {
-                        ESP_LOGE(TAG, "HANDSHAKE_RESPONSE send failed");
-                    }
-
-                    if (xSemaphoreTake(g_conn_cfg.mutex, pdMS_TO_TICKS(100)) == pdTRUE) {
-                        xEventGroupSetBits(g_system_events, EVENT_CLIENT_CONNECTED);
-                        memcpy(&g_conn_cfg.host_ip, &client_addr.sin_addr, sizeof(struct in_addr));
-                        ESP_LOGI(TAG, "Connection accepted from %s", client_ip);
-                        xSemaphoreGive(g_conn_cfg.mutex);
-                    } else {
-                        ESP_LOGW(TAG, "Failed to lock g_conn_cfg mutex");
-                    }
+                if (xSemaphoreTake(g_conn_cfg.mutex, pdMS_TO_TICKS(100)) == pdTRUE) {
+                    xEventGroupSetBits(g_system_events, EVENT_CLIENT_CONNECTED);
+                    memcpy(&g_conn_cfg.host_ip, &client_addr.sin_addr, sizeof(struct in_addr));
+                    ESP_LOGI(TAG, "Connection accepted from %s", client_ip);
+                    xSemaphoreGive(g_conn_cfg.mutex);
                 } else {
-                    ESP_LOGW(TAG, "Connection accepted from %s (not on whitelist)", client_ip);
+                    ESP_LOGW(TAG, "Failed to lock g_conn_cfg mutex");
                 }
             }
         } else if (recv_len < 0) {
@@ -285,11 +193,6 @@ void discovery_server_mgr_task(void *arg) {
     EventBits_t bits;
     TaskHandle_t handshake_hndl = NULL;
     TaskHandle_t discovery_hndl = NULL;
-    s_whitelist_mutex = xSemaphoreCreateMutex();
-    if (s_whitelist_mutex == NULL) {
-        ESP_LOGE(TAG, "Failed to create whitelist mutex");
-        return;
-    }
 
     while (1) {
         bits = xEventGroupWaitBits(
@@ -316,6 +219,4 @@ void discovery_server_mgr_task(void *arg) {
         vTaskDelete(handshake_hndl);
         vTaskDelete(discovery_hndl);
     }
-
-    vSemaphoreDelete(s_whitelist_mutex);
 }
